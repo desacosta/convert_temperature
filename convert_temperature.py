c65262fc24e265b5545d9fb87fae1b65ee448df1
desacosta <d.costa@tudelft.nl>
2023-05-25 14:23:01 +0200

    added docstring for source code documentation

--- convert_temperature.py
+++ convert_temperature.py
@@ -1,2 +1,11 @@
 def fahrenheit_to_celsius(temp_fahrenheit):
+	"""
+	Converts temperature from Fahrenheit to Celsius
+
+	Parameters:
+	temperature_fahrenheit
+
+	Returns:
+	temperature_celsius
+	"""
 	return (temp_fahrenheit-32)*(5/9)
